86fa1bae478213144a369e81c03a7720fd4878ca
wonijjang0915 <55573431+wonijjang0915@users.noreply.github.com>
2019-09-20 15:07:46 +0900

    03

--- main.c
+++ main.c
@@ -4,16 +4,16 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
 int main(int argc, char *argv[]) {
-	int x;
-	char y;
-	short z;
-	float a;
-	double b;
-	
-	printf("bytes of int : %d/n", sizeof(x));
-	printf("bytes of char : %d/n", sizeof(y));
-	printf("bytes of short : %d/n", sizeof(z));
-	printf("bytes of float : %d/n", sizeof(a));
-	printf("bytes of double : %d/n", sizeof(b));
+    int input_int;
+    float input_float;
+    
+    printf("enter an integer : ");
+    scanf("%d", &input_int);
+    
+    printf("enter an float : ");
+    scanf("%f", &input_float);
+    
+    printf("integer : %d, float : %f/n", input_int, input_float);
+    
 	return 0;
 }
